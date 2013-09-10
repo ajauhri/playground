02f73df49fddd905cc9a053f27bb148d7a4c16d8
ajauhri <abhinavjauhri@gmail.com>
2013-09-09 23:42:34 -0700

    readme add

---
+++ la/README.md
@@ -0,0 +1,5 @@
+##### Least Square Polynomial Fitting
+
+poly\_apprx\_ls.py - ![alt text](http://imgur.com/pOVD14s)
+
+
--- la/poly_approx_ls.py
+++ la/poly_approx_ls.py
@@ -18,23 +18,41 @@ def eval():
 	A = np.ones(shape=(100, 2))
 	A[:,1] = x
 	x_1 = np.linalg.lstsq(A,y)[0]
+	y_1 = np.dot(A,x_1)
 	plt.plot(x, y, 'r', label='actual curve')
-	plt.plot(x, np.dot(A,x_1), 'g--', label='first order')
+	plt.plot(x, y_1, 'g--', label='Degree: 1')
+	print "1st plot RMS=", np.sqrt(np.mean((y_1 -y)**2))
 
 	#degree 2 
 	A = np.ones(shape=(100, 3))
 	A[:,1] = x
 	A[:,2] = np.power(x, 2)
 	x_2 = np.linalg.lstsq(A,y)[0] 
-	plt.plot(x, np.dot(A, x_2), 'b--', label='second order')
+	y_2 = np.dot(A, x_2)
+	plt.plot(x, y_2, 'b--', label='Degree: 2')
+	print "1st plot RMS=", np.sqrt(np.mean((y_2 - y)**2))
 
 	#degree 3
 	A = np.ones(shape=(100, 4))
 	A[:,1] = x
-	A[:,2] = np.square(x)
+	A[:,2] = np.power(x, 2)
 	A[:,3] = np.power(x, 3)
 	x_3 = np.linalg.lstsq(A,y)[0]
-	plt.plot(x, np.dot(A, x_3), 'k--', label='third order') 
+	y_3 = np.dot(A, x_3)
+	plt.plot(x, y_3, 'k--', label='Degree: 3') 
+	print "3rd plot RMS=", np.sqrt(np.mean((y_3 - y)**2))
+
+	#degree 4
+	A = np.ones(shape=(100, 5))
+	A[:,1] = x
+	A[:,2] = np.power(x, 2)
+	A[:,3] = np.power(x, 3)
+	A[:,4] = np.power(x, 4)
+	x_4 = np.linalg.lstsq(A,y)[0]
+	y_4 = np.dot(A, x_4)
+	plt.plot(x, y_4, 'm--', label='Degree: 4') 
+	print "4th plot RMS=", np.sqrt(np.mean((y_4 - y)**2))
+
 	plt.legend() #to show labels
 	plt.show()
 
